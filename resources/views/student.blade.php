f6ea4b449394addbe5a52049762e22ddce9b687d
rohit090219 <143824617+rohit090219@users.noreply.github.com>
2026-04-09 09:27:56 +0530

    oky

---
+++ resources/views/student.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Details</title>
+</head>
+<body>
+
+<h2>Student Details</h2>
+
+<p><b>Name:</b> {{ $student['name'] }}</p>
+<p><b>Course:</b> {{ $student['course'] }}</p>
+<p><b>City:</b> {{ $student['city'] }}</p>
+
+</body>
+</html>
